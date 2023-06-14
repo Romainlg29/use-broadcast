ab3f5defee67928915b1ac67dd89f349ef83f954
Romainlg29 <31577471+Romainlg29@users.noreply.github.com>
2023-06-14 21:38:44 +0000

    Deploying to gh-pages from @ Romainlg29/use-broadcast@4f3d65d2e6f4a9499c6ae0ec5381f09e6e35eb22 🚀

---
+++ assets/useColorScheme-9e0390a4.js
@@ -0,0 +1,1 @@
+import{r as s}from"./index-f2f141d3.js";const n=()=>{const a=(()=>window.matchMedia("(prefers-color-scheme: dark)").matches)(),[c,r]=s.useState(a);return s.useEffect(()=>{const t=window.matchMedia("(prefers-color-scheme: dark)");return t.addEventListener("change",e=>r(e.matches)),()=>t.removeEventListener("change",e=>r(e.matches))},[]),c};export{n as u};
