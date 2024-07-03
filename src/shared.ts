e3d8f13cd80d21e92a79958a6cf322d1b32d9e2c
Romain <legall.romain29470@gmail.com>
2024-07-03 19:52:48 +0200

    fix: from doc

--- src/shared.ts
+++ src/shared.ts
@@ -1,6 +1,7 @@
-import { createStore, Mutate, StateCreator, StoreApi, StoreMutatorIdentifier } from 'zustand';
+import { Mutate, StateCreator, StoreApi, StoreMutatorIdentifier } from 'zustand';
 
 export type Write<T, U> = Omit<T, keyof U> & U;
+export type Cast<T, U> = T extends U ? T : U;
 
 export type SharedState = {
 	/**
@@ -38,314 +39,297 @@ export type Shared = <
 	Mps extends [StoreMutatorIdentifier, unknown][] = [],
 	Mcs extends [StoreMutatorIdentifier, unknown][] = []
 >(
-	f: StateCreator<T, [...Mps, ['shared', unknown]], Mcs>,
+	f: StateCreator<T, [...Mps, ['shared', SharedState]], Mcs>,
 	options?: SharedOptions
-) => StateCreator<T, Mps, [['shared', StoreApi<SharedState>], ...Mcs]>;
+) => StateCreator<T, Mps, [['shared', SharedState], ...Mcs]>;
 
 /**
  * Extend the Zustand store
  */
-declare module 'zustand/vanilla' {
+declare module 'zustand' {
 	interface StoreMutators<S, A> {
-		// ['shared']: Write<S, SharedState>;
-		shared: Write<S, { shared: A }>;
+		shared: Write<Cast<S, object>, { shared: A }>;
 	}
 }
+
+/**
+ * Type implementation of the Shared function
+ */
+type SharedImpl = <T>(f: StateCreator<T, [], []>, options?: SharedOptions) => StateCreator<T, [], []>;
 
 /**
  * Shared implementation
  * @param f Zustand state creator
  * @param options The options
  */
-const sharedImpl = <T>(f: StateCreator<T, [], []>, options?: SharedOptions): StateCreator<T, [], []> => {
-	const configure = (
-		set: StoreApi<T>['setState'],
-		get: StoreApi<T>['getState'],
-		store: Mutate<StoreApi<T>, [['shared', StoreApi<SharedState>]]>
-	) => {
-		if (typeof window === 'undefined') {
-			console.warn('BroadcastChannel is not supported in this environment. The store will not be shared.');
-			return f(set, get, store);
+const sharedImpl: SharedImpl = (f, options) => (set, get, _store) => {
+	if (typeof window === 'undefined') {
+		console.warn('BroadcastChannel is not supported in this environment. The store will not be shared.');
+		return f(set, get, _store);
+	}
+
+	/**
+	 * If BroadcastChannel is not supported, return the basic store
+	 */
+	if (typeof BroadcastChannel === 'undefined') {
+		console.warn('BroadcastChannel is not supported in this browser. The store will not be shared.');
+		return f(set, get, _store);
+	}
+
+	/**
+	 * Types
+	 */
+	type T = ReturnType<typeof f>;
+
+	type Item = { [key: string]: unknown };
+	type Message =
+		| {
+				action: 'sync';
+		  }
+		| {
+				action: 'change';
+				state: Item;
+		  }
+		| {
+				action: 'add_new_tab';
+				id: number;
+		  }
+		| {
+				action: 'close';
+				id: number;
+		  }
+		| {
+				action: 'change_main';
+				id: number;
+				tabs: number[];
+		  };
+
+	const store = _store as Mutate<StoreApi<T>, [['shared', SharedState]]>;
+
+	/**
+	 * Is the store synced with the other tabs
+	 */
+	let isSynced = get() !== undefined;
+
+	/**
+	 * Is this tab / window the main tab / window
+	 * When a new tab / window is opened, it will be synced with the main
+	 */
+	let isMain = false;
+
+	/**
+	 * The broadcast channel name
+	 */
+	const name = options?.name ?? f.toString();
+
+	/**
+	 * The id of the tab / window
+	 */
+	let id = 0;
+
+	/**
+	 * Store a list of all the tabs / windows
+	 * Only for the main tab / window
+	 */
+	const tabs: number[] = [0];
+
+	/**
+	 * Create the broadcast channel
+	 */
+	const channel = new BroadcastChannel(name);
+
+	/**
+	 * Handle the Zustand set function
+	 * Trigger a postMessage to all the other tabs
+	 */
+	const onSet: typeof set = (...args) => {
+		/**
+		 * Get the previous states
+		 */
+		const previous = get() as Item;
+
+		/**
+		 * Update the states
+		 */
+		set(...args);
+
+		/**
+		 * If the stores should not be synced, return.
+		 */
+		if (options?.unsync) {
+			return;
 		}
 
 		/**
-		 * If BroadcastChannel is not supported, return the basic store
+		 * Get the fresh states
 		 */
-		if (typeof BroadcastChannel === 'undefined') {
-			console.warn('BroadcastChannel is not supported in this browser. The store will not be shared.');
-			return f(set, get, store);
-		}
+		const updated = get() as Item;
 
 		/**
-		 * Types
+		 * Get the states that changed
 		 */
-		type Item = { [key: string]: unknown };
-		type Message =
-			| {
-					action: 'sync';
-			  }
-			| {
-					action: 'change';
-					state: Item;
-			  }
-			| {
-					action: 'add_new_tab';
-					id: number;
-			  }
-			| {
-					action: 'close';
-					id: number;
-			  }
-			| {
-					action: 'change_main';
-					id: number;
-					tabs: number[];
-			  };
+		const state = Object.entries(updated).reduce((obj, [key, val]) => {
+			if (previous[key] !== val) {
+				obj = { ...obj, [key]: val };
+			}
+			return obj;
+		}, {} as Item);
 
 		/**
-		 * Is the store synced with the other tabs
+		 * Send the states to all the other tabs
 		 */
-		let isSynced = get() !== undefined;
+		channel.postMessage({ action: 'change', state } as Message);
+	};
 
-		/**
-		 * Is this tab / window the main tab / window
-		 * When a new tab / window is opened, it will be synced with the main
-		 */
-		let isMain = false;
-
-		/**
-		 * The broadcast channel name
-		 */
-		const name = options?.name ?? f.toString();
-
-		/**
-		 * The id of the tab / window
-		 */
-		let id = 0;
-
-		/**
-		 * Store a list of all the tabs / windows
-		 * Only for the main tab / window
-		 */
-		const tabs: number[] = [0];
-
-		/**
-		 * Create the broadcast channel
-		 */
-		const channel = new BroadcastChannel(name);
-
-		/**
-		 * Extended store type
-		 */
-		// type ExtendedStore = Mutate<typeof store, [['shared', StoreApi<SharedState>]]>;
-
-		/**
-		 * Handle the shared store
-		 */
-
-		// (store as ExtendedStore).shared = createStore<SharedState>(() => ({
-		// 	isMain,
-		// }));
-
-		/**
-		 * Handle the Zustand set function
-		 * Trigger a postMessage to all the other tabs
-		 */
-		const onSet: typeof set = (...args) => {
+	/**
+	 * Subscribe to the broadcast channel
+	 */
+	channel.onmessage = (e) => {
+		if ((e.data as Message).action === 'sync') {
 			/**
-			 * Get the previous states
+			 * If this tab / window is not the main, return
 			 */
-			const previous = get() as Item;
-
-			/**
-			 * Update the states
-			 */
-			set(...args);
-
-			/**
-			 * If the stores should not be synced, return.
-			 */
-			if (options?.unsync) {
+			if (!isMain) {
 				return;
 			}
 
 			/**
-			 * Get the fresh states
+			 * Remove all the functions and symbols from the store
 			 */
-			const updated = get() as Item;
-
-			/**
-			 * Get the states that changed
-			 */
-			const state = Object.entries(updated).reduce((obj, [key, val]) => {
-				if (previous[key] !== val) {
+			const state = Object.entries(get() as Item).reduce((obj, [key, val]) => {
+				if (typeof val !== 'function' && typeof val !== 'symbol') {
 					obj = { ...obj, [key]: val };
 				}
 				return obj;
-			}, {} as Item);
+			}, {});
 
 			/**
-			 * Send the states to all the other tabs
+			 * Send the state to the other tabs
 			 */
 			channel.postMessage({ action: 'change', state } as Message);
-		};
-
-		/**
-		 * Subscribe to the broadcast channel
-		 */
-		channel.onmessage = (e) => {
-			if ((e.data as Message).action === 'sync') {
-				/**
-				 * If this tab / window is not the main, return
-				 */
-				if (!isMain) {
-					return;
-				}
-
-				/**
-				 * Remove all the functions and symbols from the store
-				 */
-				const state = Object.entries(get() as Item).reduce((obj, [key, val]) => {
-					if (typeof val !== 'function' && typeof val !== 'symbol') {
-						obj = { ...obj, [key]: val };
-					}
-					return obj;
-				}, {});
-
-				/**
-				 * Send the state to the other tabs
-				 */
-				channel.postMessage({ action: 'change', state } as Message);
-
-				/**
-				 * Set the new tab / window id
-				 */
-				const new_id = tabs[tabs.length - 1]! + 1;
-				tabs.push(new_id);
-
-				channel.postMessage({ action: 'add_new_tab', id: new_id } as Message);
-
-				return;
-			}
 
 			/**
-			 * Set an id for the tab / window if it doesn't have one
+			 * Set the new tab / window id
 			 */
-			if ((e.data as Message).action === 'add_new_tab' && !isMain && id === 0) {
-				id = e.data.id;
-				return;
-			}
+			const new_id = tabs[tabs.length - 1]! + 1;
+			tabs.push(new_id);
 
-			/**
-			 * On receiving a new state, update the state
-			 */
-			if ((e.data as Message).action === 'change') {
-				/**
-				 * Update the state
-				 */
-				set(e.data.state);
+			channel.postMessage({ action: 'add_new_tab', id: new_id } as Message);
 
-				/**
-				 * Set the synced attribute
-				 */
-				isSynced = true;
-			}
-
-			/**
-			 * On receiving a close message, remove the tab / window id from the list
-			 */
-			if ((e.data as Message).action === 'close') {
-				if (!isMain) {
-					return;
-				}
-
-				const index = tabs.indexOf(e.data.id);
-				if (index !== -1) {
-					tabs.splice(index, 1);
-				}
-			}
-
-			/**
-			 * On receiving a change_main message, change the main tab / window
-			 */
-			if ((e.data as Message).action === 'change_main') {
-				if (e.data.id === id) {
-					isMain = true;
-					// (store as ExtendedStore).shared.setState({ isMain });
-
-					tabs.splice(0, tabs.length, ...e.data.tabs);
-				}
-			}
-		};
-
-		/**
-		 * Synchronize with the main tab
-		 */
-		const synchronize = (): void => {
-			channel.postMessage({ action: 'sync' } as Message);
-
-			/**
-			 * If isSynced is false after 100ms, this tab is the main tab
-			 */
-			setTimeout(() => {
-				if (!isSynced) {
-					isMain = true;
-					// (store as ExtendedStore).shared.setState({ isMain });
-
-					isSynced = true;
-				}
-			}, options?.mainTimeout ?? 100);
-		};
-
-		/**
-		 * Handle case when the tab / window is closed
-		 */
-		const onClose = (): void => {
-			channel.postMessage({ action: 'close', id } as Message);
-
-			/**
-			 * If we're closing the main, make the second the new main
-			 */
-			if (isMain) {
-				/**
-				 * If there is only one tab left, close the channel and return
-				 */
-				if (tabs.length === 1) {
-					/**
-					 * Clean up
-					 */
-					channel.close();
-					return;
-				}
-
-				const remaining_tabs = tabs.filter((tab) => tab !== id);
-				channel.postMessage({ action: 'change_main', id: remaining_tabs[0], tabs: remaining_tabs } as Message);
-
-				return;
-			}
-		};
-
-		/**
-		 * Add close event listener
-		 */
-		window.addEventListener('beforeunload', onClose);
-
-		/**
-		 * Synchronize with the main tab
-		 */
-		if (!isSynced) {
-			synchronize();
+			return;
 		}
 
 		/**
-		 * Modify and return the Zustand store
+		 * Set an id for the tab / window if it doesn't have one
 		 */
-		store.setState = onSet;
+		if ((e.data as Message).action === 'add_new_tab' && !isMain && id === 0) {
+			id = e.data.id;
+			return;
+		}
 
-		return f(onSet, get, store);
+		/**
+		 * On receiving a new state, update the state
+		 */
+		if ((e.data as Message).action === 'change') {
+			/**
+			 * Update the state
+			 */
+			set(e.data.state);
+
+			/**
+			 * Set the synced attribute
+			 */
+			isSynced = true;
+		}
+
+		/**
+		 * On receiving a close message, remove the tab / window id from the list
+		 */
+		if ((e.data as Message).action === 'close') {
+			if (!isMain) {
+				return;
+			}
+
+			const index = tabs.indexOf(e.data.id);
+			if (index !== -1) {
+				tabs.splice(index, 1);
+			}
+		}
+
+		/**
+		 * On receiving a change_main message, change the main tab / window
+		 */
+		if ((e.data as Message).action === 'change_main') {
+			if (e.data.id === id) {
+				isMain = true;
+				tabs.splice(0, tabs.length, ...e.data.tabs);
+			}
+		}
 	};
 
-	return configure as StateCreator<T, [], []>;
+	/**
+	 * Synchronize with the main tab
+	 */
+	const synchronize = (): void => {
+		channel.postMessage({ action: 'sync' } as Message);
+
+		/**
+		 * If isSynced is false after 100ms, this tab is the main tab
+		 */
+		setTimeout(() => {
+			if (!isSynced) {
+				isMain = true;
+				isSynced = true;
+			}
+		}, options?.mainTimeout ?? 100);
+	};
+
+	/**
+	 * Handle case when the tab / window is closed
+	 */
+	const onClose = (): void => {
+		channel.postMessage({ action: 'close', id } as Message);
+
+		/**
+		 * If we're closing the main, make the second the new main
+		 */
+		if (isMain) {
+			/**
+			 * If there is only one tab left, close the channel and return
+			 */
+			if (tabs.length === 1) {
+				/**
+				 * Clean up
+				 */
+				channel.close();
+				return;
+			}
+
+			const remaining_tabs = tabs.filter((tab) => tab !== id);
+			channel.postMessage({ action: 'change_main', id: remaining_tabs[0], tabs: remaining_tabs } as Message);
+
+			return;
+		}
+	};
+
+	/**
+	 * Add close event listener
+	 */
+	window.addEventListener('beforeunload', onClose);
+
+	/**
+	 * Synchronize with the main tab
+	 */
+	if (!isSynced) {
+		synchronize();
+	}
+
+	/**
+	 * Modify and return the Zustand store
+	 */
+	store.setState = onSet;
+
+	return f(onSet, get, store);
 };
 
 /**
