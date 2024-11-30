38f91098de4cf89a4cb97f88993330b3c6f9fa7f
Ken <etchellsken@gmail.com>
2024-11-30 17:33:59 -0500

    Revert example

--- example/src/stores/useCountStore.ts
+++ example/src/stores/useCountStore.ts
@@ -1,5 +1,4 @@
-// import { shared } from 'use-broadcast-ts';
-import { shared } from '../../../src/shared';
+import { shared } from 'use-broadcast-ts';
 import { createWithEqualityFn as create } from 'zustand/traditional';
 
 type CountStore = {
@@ -7,49 +6,20 @@ type CountStore = {
 	increment: () => void;
 	decrement: () => void;
 
-	nestedObj: object,
-
-	bigCount: bigint,
-
 	mode: 'Sync' | 'Not Sync';
 	setMode: (mode: 'Sync' | 'Not Sync') => void;
 };
-
-const merge = (state: CountStore, receivedState: Partial<CountStore>) => {
-	const newState = {
-		...state,
-		count: receivedState.count,
-		nestedObj: {
-			...state.nestedObj,
-			innerCount: receivedState?.nestedObj?.innerCount,
-		}
-	}
-
-	return newState
-}
 
 export const useCountStore = create<CountStore>()(
 	shared(
 		(set, get) => ({
 			count: 0,
-			increment: () => set((s) => ({ count: s.count + 1, bigCount: s.bigCount + 1n, nestedObj: { ...s.nestedObj, innerCount: s.nestedObj.innerCount += 1 } })),
-			decrement: () => set((s) => ({ count: s.count - 1, bigCount: s.bigCount - 1n, nestedObj: { ...s.nestedObj, innerCount: s.nestedObj.innerCount -= 1 } })),
-
-			nestedObj: {
-				// innerFunc: () => console.log("This shouldn't cause errors"),
-				// innerSymbol: Symbol("hi"),
-				innerCount: 0,
-			},
-
-			bigCount: BigInt(Number.MAX_SAFE_INTEGER)*100n,
+			increment: () => set((s) => ({ count: s.count + 1 })),
+			decrement: () => set((s) => ({ count: s.count - 1 })),
 
 			mode: 'Sync',
 			setMode: (mode) => set({ mode }),
 		}),
-		{ 
-			name: 'my-store',
-			partialize: (state) => ({count: state.count, nestedObj: state.nestedObj}),
-			merge: merge,
-		 }
+		{ name: 'my-store' }
 	)
 );
